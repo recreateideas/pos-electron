c849b837f9abff0eaec8e37f5182c2a7be4b28ea
cladea.au@gmail.com <cladea.au@gmail.com>
2021-05-23 15:10:18 +1000

    fix: added linkedin to homepage

--- pos-homepage/src/App.js
+++ pos-homepage/src/App.js
@@ -1,13 +1,20 @@
 import logo from './assets/hamburger.png';
 import github from './assets/github.png';
 import download from './assets/download.png';
+import linkedin from './assets/linkedin.png';
+
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img
+                    src={logo}
+                    className="App-logo"
+                    alt="logo"
+                    style={{ height: 200, width: 'auto' }}
+                />
                 <h2>Electron Pos</h2>
                 <div className="links-wrap">
                     <a
@@ -15,6 +22,7 @@ function App() {
                         href="https://github.com/recreateideas/pos-electron"
                         target="_blank"
                         rel="noopener noreferrer"
+                        style={{ marginBottom: 0 }}
                     >
                         <div className="links">
                             <img
@@ -29,7 +37,6 @@ function App() {
                             <div className="text">Github Repo</div>
                         </div>
                     </a>
-
                     <a
                         className="link"
                         href="https://claudio-electron-builds.s3-ap-southeast-2.amazonaws.com/Pos-0.10.0.dmg"
@@ -48,7 +55,29 @@ function App() {
                         </div>
                     </a>
                 </div>
-                <p style={{ marginTop: 24, fontSize: 16 }}>
+
+                <a
+                    className="link"
+                    href="https://www.linkedin.com/in/recreateideas/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ marginTop: 24 }}
+                >
+                    <div className="links">
+                        <img
+                            src={linkedin}
+                            style={{
+                                height: 48,
+                                width: 48,
+                                borderRadius: '100%'
+                            }}
+                            alt="linkedin"
+                        />
+                        <div className="text">my Linkedin</div>
+                    </div>
+                </a>
+
+                <p style={{ fontSize: 16 }}>
                     <b>If the app is blocked by macOS</b>:<br />{' '}
                     <u>
                         System Preferences -&gt; Security & Privacy -&gt;
